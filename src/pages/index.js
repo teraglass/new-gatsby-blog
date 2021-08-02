9df3e28166c614c263745b99299cf43a7668b2d4
tera <thecodss@gmail.com>
2021-08-02 23:04:20 +0900

    added all gatsby-blog files

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: purple;
+`
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        <h1>Tera's Thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <span>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </span>
+              <p>{node.excerpt}</p>
+            </BlogLink>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort:{fields:[fronstmatter___date, order:DESC]}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields{
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
